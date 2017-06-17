63dbc8f49c78f3c7c220714f1697b8f28e1f8f26
yahmds <yahmdsaleh@gmail.com>
2017-06-17 21:18:56 +0200

    Create Middleware classes dynamically

--- lib/space_station.rb
+++ lib/space_station.rb
@@ -3,50 +3,29 @@ require "space_station/version"
 module SpaceStation
   ASCII_PATH = File.join(File.dirname(__FILE__), '/space_station/ascii/').freeze
 
-  class SpaceStation
+  class Station
     def initialize(app)
-      @app = app
+      @app       = app
+      @ascii_art =
+        self.class.name.split('::').last.gsub(/([a-z\d])([A-Z])/,'\1_\2').downcase
     end
 
     def call(env)
-      write_before_response
+      log_before_response(@ascii_art)
 
       @app.call(env)
     end
 
-    def write_before_response
-      ""
+    protected
+
+    def log_before_response(to_write = @ascii_art)
+      puts IO.read("#{SpaceStation::ASCII_PATH}/#{to_write}")
     end
   end
 
-  def self.define_all
-    # Dir.entries(ASCII_PATH).select { |f| !File.directory? f }.each do |ascii_file|
-    #   klass = Class.new(SpaceStation::SpaceStation) do
-    #     def write_before_response
-    #       puts IO.read("#{ASCII_PATH}/#{ascii_file}")
-    #     end
-    #   end
-    #
-    #   SpaceStation.const_set(ascii_file.to_s.split('_').collect(&:capitalize).join, klass)
-    # end
+  klass = Class.new(SpaceStation::Station)
+  Dir.entries(ASCII_PATH).select { |f| !File.directory? f }.each do |ascii_file|
+    klass_name = ascii_file.to_s.split('_').collect(&:capitalize).join
+    SpaceStation.const_set(klass_name, klass)
   end
 end
-
-Dir.entries(SpaceStation::ASCII_PATH).select { |f| !File.directory? f }.each do |ascii_file|
-  klass = Class.new(SpaceStation::SpaceStation) do
-    def write_before_response
-      puts IO.read("#{SpaceStation::ASCII_PATH}/#{ascii_file}")
-    end
-  end
-
-  SpaceStation.const_set(ascii_file.to_s.split('_').collect(&:capitalize).join, klass)
-end
-
-# require 'space_station/airplane'
-# require 'space_station/apollo'
-# require 'space_station/darth_vader'
-# require 'space_station/f16'
-# require 'space_station/middle_finger'
-# require 'space_station/space_shuttle'
-# require 'space_station/ufo'
-# require 'space_station/unicorn'
